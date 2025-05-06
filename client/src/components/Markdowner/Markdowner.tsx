c976325172d59a669006f1f7e296f97f1664bdbe
Charlytoc <charlyjchaconc@gmail.com>
2025-05-05 22:29:21 -0500

    add styling for easier reading in dev client

--- client/src/components/Markdowner/Markdowner.tsx
+++ client/src/components/Markdowner/Markdowner.tsx
@@ -3,33 +3,35 @@ import remarkGfm from "remark-gfm";
 
 export const Markdowner = ({ markdown }: { markdown: string }) => {
   return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      components={{
-        a: ({ node, ...props }) => {
-          return <a {...props} target="_blank" rel="noopener noreferrer" />;
-        },
-        code: ({ node, ...props }) => {
-          const isAnalysis = props.className?.includes("analysis");
-          if (isAnalysis) {
-            return (
-              <blockquote className="bg-gray-100 p-2 rounded-md">
-                Análisis desde la palabra:{" "}
-                {props.children?.toString().split("-")[0]} hasta la palabra:{" "}
-                {props.children?.toString().split("-")[1]}
-              </blockquote>
-            );
-          }
+    <div className="markdown-container">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          a: ({ node, ...props }) => {
+            return <a {...props} target="_blank" rel="noopener noreferrer" />;
+          },
+          code: ({ node, ...props }) => {
+            const isAnalysis = props.className?.includes("analysis");
+            if (isAnalysis) {
+              return (
+                <blockquote className="bg-gray-100 p-2 rounded-md">
+                  Análisis desde la palabra:{" "}
+                  {props.children?.toString().split("-")[0]} hasta la palabra:{" "}
+                  {props.children?.toString().split("-")[1]}
+                </blockquote>
+              );
+            }
 
-          return (
-            <code {...props} className={`bg-gray-100 p-2 rounded-md`}>
-              {props.children}
-            </code>
-          );
-        },
-      }}
-    >
-      {markdown}
-    </Markdown>
+            return (
+              <code {...props} className={`bg-gray-100 p-2 rounded-md`}>
+                {props.children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </Markdown>
+    </div>
   );
 };
